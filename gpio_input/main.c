682f27724212b7c4e0dbf70a1ee83aa2746c64e2
Jacob C <>
2020-05-24 19:41:20 +0200

    added push button input with debouncer

--- gpio_input/main.c
+++ gpio_input/main.c
@@ -1,21 +1,64 @@
 #include "stm32f10x.h"
+#include "stm32f10x_misc.h"
+
 #include "stm32f10x_init.h"
+#include "printf.h"
+
+void __io_putchar(void* p, char ch)
+{
+    /* Write a character to the USART */
+    USART_SendData(USART1, (uint8_t) ch);
+
+    /* Loop until the end of transmission */
+    while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET) {}
+}
+
+
 int main(void)
 {
     /* Initialize system */
     STM32_Configuration();
+    char button_pressed = 0;
+    int button_pressed_conf_level = 0;
+    int button_released_conf_level = 0;
+    char LED_state = 0;
+    int confidence_thresh = 300;
 
-    while (1)
-    {
-    // turn on the LED when button is pressed
-    // if the button is pressed (IDR input data register)
+    while (1) {
+        // turn on the LED when button is pressed
+        // if the button is pressed (IDR input data register)
         if (GPIOA->IDR & GPIO_Pin_0) {
-            GPIO_WriteBit(GPIOC, GPIO_Pin_9, Bit_SET);
+            if (!button_pressed) {
+                // Increase button pressed confidence
+                if (button_pressed_conf_level > confidence_thresh) {
+                    if (!LED_state) {
+                        GPIO_WriteBit(GPIOC, GPIO_Pin_9, Bit_SET);
+                    }
+                    else {
+                        GPIO_WriteBit(GPIOC, GPIO_Pin_9, Bit_RESET);
+                    }
+                    LED_state = !LED_state;
+                    button_pressed = 1;
+                }
+                else {
+                    button_pressed_conf_level++;
+                    button_released_conf_level = 0;
+                }            
+            }
         }
         else {
-            GPIO_WriteBit(GPIOC, GPIO_Pin_9, Bit_RESET);
+
+            if (button_pressed) {
+                // Increase button pressed confidence
+                if (button_released_conf_level > confidence_thresh) {
+                    button_pressed = 0;
+                }
+                else {
+                    button_released_conf_level++;
+                    button_pressed_conf_level = 0;
+                }            
+            }
         }
 
     }
-
 }
--- gpio_input/stm32f10x_init.c
+++ gpio_input/stm32f10x_init.c
@@ -9,6 +9,9 @@ void STM32_Configuration(void)
     /* GPIO Configuration */
     GPIO_Configuration();
 
+    /* Set USART Parameters */
+    USART_Configuration();
+
     /* Set SysTick to 1ms */
     SysTick_Config(SystemCoreClock / 1000);
 }
@@ -20,6 +23,9 @@ void RCC_Configuration(void)
 
     /* GPIOA clock enable (USER Button) */
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+
+    /* GPIOA clock enable (USART1) */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_USART1, ENABLE);
 
 }
 
@@ -45,3 +51,35 @@ void GPIO_Configuration(void)
 
 }
 
+
+void USART_Configuration(void)
+{
+    USART_InitTypeDef USART_InitStructure;
+
+    /* USART1 configured as follow:
+          - BaudRate = 9600 baud
+          - Word Length = 8 Bits
+          - Two Stop Bit
+          - No parity
+          - Hardware flow control disabled (RTS and CTS signals)
+          - Receive and transmit enabled
+    */
+    USART_InitStructure.USART_BaudRate = 9600;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Tx;
+
+    USART_Init(USART1, &USART_InitStructure);
+
+    /* Enable the USART1 Transmit interrupt: this interrupt is generated when the
+       USART1 transmit data register is empty */
+    //USART_ITConfig(USART1, USART_IT_TXE, ENABLE);
+
+    /* Enable the USART1 Receive interrupt: this interrupt is generated when the
+       USART1 receive data register is not empty */
+    //USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+
+    USART_Cmd(USART1, ENABLE);
+}
